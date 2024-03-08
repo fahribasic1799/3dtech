f9aa3c358ce8facc4eec27c28a38916cf2c2a00e
oja <obajasiadari@gmail.com>
2024-03-08 09:37:17 +0700

    tambah phpmailer, ubah konsep kontak, dan debug preloader

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,12 @@
 <?php
-//konektor database
+//Aktivasi PHPMailer-->jangan dihapus
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//koneksi database
 include '../koneksi.php';
 
 //menangkapdata yang di kirim dari form
@@ -9,7 +16,44 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 
 //menginput data ke database
-mysqli_query($koneksi,"insert into contact2 values('','$name','$email','$subject','$message')");
+mysqli_query($koneksi,"insert into contact values('','$name','$email','$subject','$message')");
 
-//mengalihkan halaman kembali ke index.html
-header("location:../index.html");
+//Load Composer's autoloader -->setup autoload
+require '../vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'mailingtestingobaja@gmail.com';                     //SMTP username
+    $mail->Password   = 'hnav skyd ophk lgie';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('obajasiadari@gmail.com', 'Chris');     //Add a recipient
+    //$mail->addReplyTo($email, $name);
+
+    //Attachments
+    //$mail->addAttachment('assets/3dtec.png');         //Add attachments
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    $mail->AltBody = '';
+
+    $mail->send();
+    echo 'Message has been sent';
+    
+
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+header('Location: ../thankyou.php');
+exit;
---
+++ thankyou.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html lang="id-ID">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>3D TECH - PENYEDIA LAYANAN INTERNET</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/3dtec.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Arsha
+  * Updated: Jan 29 2024 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+ 
+  <h1>Thank You ! for contacting us</h1>
+  <a href="index.html">Back</a>
+  
+
+</body>
+
+</html>
